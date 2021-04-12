bf76f8ab46f601eee455a519a6ea45e7deb655ec
anhdunyb99 <anhdunyb99@gmail.com>
2021-04-12 13:16:57 +0700

    add ClearButton and NegativeButton

--- src/components/NumberProvider.js
+++ src/components/NumberProvider.js
@@ -18,7 +18,9 @@ const NumberProvider = props => {
   };
 
   const handleClearValue = () => {
-
+    setNumber('');
+    setStoredNumber('');
+    setFunctionType('');
   };
 
   const handleBackButton = () => {
@@ -30,7 +32,19 @@ const NumberProvider = props => {
   };
 
   const handleToggleNegative = () => {
-
+    if (number) {
+      if (number > 0) {
+        setNumber(`-${number}`);
+      } else {
+        const positiveNumber = number.slice(1);
+        setNumber(positiveNumber);
+      }
+    } else if (storedNumber > 0) {
+      setStoredNumber(`-${storedNumber}`);
+    } else {
+      const positiveNumber = storedNumber.slice(1);
+      setStoredNumber(positiveNumber);
+    }
   };
 
   const doMath = () => {
